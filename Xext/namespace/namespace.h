bc4c35d04587868a006af940bc7c89dffb6ae19b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-01-25 16:38:10 +0100

    Xnamespace: add basic namespace config structures
    Adding data structure and initial data for namespace configuration.
    
    Built-in namespaces are ROOT and ANONYMOUS.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/namespace/namespace.h
+++ Xext/namespace/namespace.h
@@ -4,6 +4,27 @@
 #include <stdio.h>
 #include <X11/Xmd.h>
 
+#include "include/list.h"
+#include "include/window.h"
+
+struct Xnamespace {
+    struct xorg_list entry;
+    const char *name;
+    Bool builtin;
+    Bool superPower;
+    size_t refcnt;
+};
+
+extern struct xorg_list ns_list;
+extern struct Xnamespace ns_root;
+extern struct Xnamespace ns_anon;
+
+#define NS_NAME_ROOT      "root"
+#define NS_NAME_ANONYMOUS "anon"
+
+Bool XnsLoadConfig(void);
+struct Xnamespace *XnsFindByName(const char* name);
+
 #define XNS_LOG(...) do { printf("XNS "); printf(__VA_ARGS__); } while (0)
 
 static inline Bool streq(const char *a, const char *b)
