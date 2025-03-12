824a78e8fc3485757a21572632c980d7b0229295
Enrico Weigelt, metux IT consult <info@metux.net>
2025-03-12 15:43:10 +0100

    dix: add dixAllocServerXID()
    Adding a separate function for allocating server-client's XIDs.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- randr/rrprovider.c
+++ randr/rrprovider.c
@@ -401,7 +401,7 @@ RRProviderCreate(ScreenPtr pScreen, const char *name,
     if (!provider)
         return NULL;
 
-    provider->id = FakeClientID(0);
+    provider->id = dixAllocServerXID();
     provider->pScreen = pScreen;
     provider->name = (char *) (provider + 1);
     provider->nameLength = nameLength;
