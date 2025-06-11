8650028e59af9bcff0e7aba751a3f820756d20a6
Alan Coopersmith <alan.coopersmith@oracle.com>
2025-06-11 13:37:57 -0700

    Revert "randr: ProcRRQueryOutputProperty(): use SwapShort()/SwapLong()"
    This reverts commit 73467faeb26b2fb628b7c6980c4573c27e0ee955.
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2012>

--- randr/rrproperty.c
+++ randr/rrproperty.c
@@ -495,16 +495,14 @@ ProcRRQueryOutputProperty(ClientPtr client)
         swaps(&rep.sequenceNumber);
         swapl(&rep.length);
     }
+    WriteToClient(client, sizeof(xRRQueryOutputPropertyReply), &rep);
     if (prop->num_valid) {
         memcpy(extra, prop->valid_values, prop->num_valid * sizeof(INT32));
-        if (client->swapped)
-            SwapLongs((CARD32*)extra, prop->num_valid);
+        client->pSwapReplyFunc = (ReplySwapPtr) Swap32Write;
+        WriteSwappedDataToClient(client, prop->num_valid * sizeof(INT32),
+                                 extra);
+        free(extra);
     }
-
-    WriteToClient(client, sizeof(xRRQueryOutputPropertyReply), &rep);
-    WriteToClient(client, prop->num_valid * sizeof(INT32), extra);
-    free(extra);
-
     return Success;
 }
 
