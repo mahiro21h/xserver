96edb55b95bddc1182058a833ae5dd233b13d93a
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-04 16:55:25 +0200

    ramdr: drop using HAVE_DIX_CONFIG_H
    This symbol is always defined, and the header is always present,
    so no need to check for it.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- randr/randrstr.h
+++ randr/randrstr.h
@@ -25,13 +25,10 @@
  * Author:  Jim Gettys, Hewlett-Packard Company, Inc.
  *	    Keith Packard, Intel Corporation
  */
-
-#ifdef HAVE_DIX_CONFIG_H
-#include <dix-config.h>
-#endif
-
 #ifndef _RANDRSTR_H_
 #define _RANDRSTR_H_
+
+#include <dix-config.h>
 
 #include <X11/X.h>
 #include <X11/Xproto.h>
