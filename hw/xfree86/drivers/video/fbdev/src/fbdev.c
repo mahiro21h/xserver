546f2c7ca03ff95df2d1b0d000099abef10c7c35
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-22 18:28:44 +0200

    xfree86: drivers: fbdev: drop XFree86Loader
    Since we're in-tree now, we always have the xf86 loader.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/drivers/video/fbdev/src/fbdev.c
+++ hw/xfree86/drivers/video/fbdev/src/fbdev.c
@@ -128,8 +128,6 @@ static const OptionInfoRec FBDevOptions[] = {
 
 /* -------------------------------------------------------------------- */
 
-#ifdef XFree86LOADER
-
 MODULESETUPPROTO(FBDevSetup);
 
 static XF86ModuleVersionInfo FBDevVersRec =
@@ -161,8 +159,6 @@ void *FBDevSetup(void *module, void *opts, int *errmaj, int *errmin)
 		return NULL;
 	}
 }
-
-#endif /* XFree86LOADER */
 
 /* -------------------------------------------------------------------- */
 /* our private data, and two functions to allocate/free this            */
