d3b01f7200ceae998bcb78efb68f85fce644a8a5
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-22 18:48:17 +0200

    xfree86: drivers: apm: drop guard on HAVE_XEXTPROTO_71
    We always have xorg-proto >= 7.1
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/drivers/video/apm/src/apm_driver.c
+++ hw/xfree86/drivers/video/apm/src/apm_driver.c
@@ -10,13 +10,7 @@
 #include "vbe.h"
 
 #include "opaque.h"
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
-
 
 #define APM_VERSION		4000
 #define APM_NAME		"APM"
