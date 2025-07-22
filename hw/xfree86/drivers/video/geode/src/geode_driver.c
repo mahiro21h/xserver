f3c48ffaf7656a7ffdba4713968a18dd83bcfab1
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-22 17:32:05 +0200

    xfree86: drivers: geode: drop guard on HAVE_XEXTPROTO_71
    We always have xorg-proto >= 7.1
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/drivers/video/geode/src/geode_driver.c
+++ hw/xfree86/drivers/video/geode/src/geode_driver.c
@@ -58,13 +58,7 @@
 #ifdef DPMSExtension
 #include "globals.h"
 #include "opaque.h"
-#ifdef HAVE_XEXTPROTO_71
 #include <X11/extensions/dpmsconst.h>
-#else
-#define DPMS_SERVER
-#include <X11/extensions/dpms.h>
-#endif
-
 #endif                          /* DPMSExtension */
 
 /* A few things all drivers should have */
