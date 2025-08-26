1cff4cae671fb521c6664a1192197a45b50f3f4b
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-26 19:02:46 +0200

    .github: update piglit version
    Update piglit version used within the test stage to current master.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/install-prereq.sh
+++ .github/scripts/install-prereq.sh
@@ -18,7 +18,7 @@ build_ac      xset              https://gitlab.freedesktop.org/xorg/app/xset    
 fi
 build_ac_xts  xts               https://gitlab.freedesktop.org/xorg/test/xts             12a887c2c72c4258962b56ced7b0aec782f1ffed
 
-clone_source piglit             https://gitlab.freedesktop.org/mesa/piglit               265896c86f90cb72e8f218ba6a3617fca8b9a1e3
+clone_source piglit             https://gitlab.freedesktop.org/mesa/piglit               28d1349844eacda869f0f82f551bcd4ac0c4edfe
 
 echo '[xts]' > piglit/piglit.conf
 echo "path=$X11_BUILD_DIR/xts" >> piglit/piglit.conf
