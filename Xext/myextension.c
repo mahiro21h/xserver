195b6d2a2d610d79fdee4122db488b3f43454ff9
Raysan Alawami <raysanalawami@gmail.com>
2025-08-31 21:30:40 +0300

    add functions to register and unregister screenlocker

--- Xext/myextension.c
+++ Xext/myextension.c
@@ -1,3 +1,5 @@
+#include <X11/Xfuncproto.h>
+#include <X11/Xmd.h>
 #include <dix-config.h>
 
 #include <stdio.h>
@@ -10,11 +12,19 @@
 #include "misc.h"
 #include "os.h"
 #include "protocol-versions.h"
+#include <pthread.h>
+#include <sys/wait.h>
+
+#define EXEC_PATH_MAX (255) /* includes null character */
 
 Bool noMyextensionExtension = FALSE;
 
 static int lockscreen(ClientPtr client);
 static int unlockscreen(ClientPtr client);
+
+static char * exec_path = NULL;
+static pthread_t thread_id = -1;
+static pid_t proc_id = -1;
 
 static int
 ProcMyextensionQueryVersion(ClientPtr client)
@@ -68,6 +78,112 @@ unlockscreen(ClientPtr client) {
     return Success;
 }
 
+static void *
+restart_client(_X_UNUSED void * vargp) {
+    int err = pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+    if (err != 0)
+        LogMessage(X_ERROR, "%s: failed to set cancel type\n", __func__);
+
+    /* if (proc_id != -1) */
+    /*     wait_process(proc_id); /\* wait for original process *\/ */
+
+    while (1) {
+        switch(fork()) {
+        case -1:
+            /* TODO: try again */
+            LogMessage(X_ERROR, "fork failed\n");
+            goto leave;
+
+            break;
+        case 0:
+            /* child */
+            execvp(exec_path, (char *[]){exec_path, NULL});
+            LogMessage(X_ERROR, "execvp failed\n");
+            exit(0);
+        default:
+            wait(NULL);
+        }
+    }
+
+leave:
+    return NULL;
+}
+
+static int
+ProcMyextensionRegisterScreenLocker(ClientPtr client) {
+    REQUEST(xMyextensionRegisterScreenLockerReq);
+
+    LogMessage(X_INFO, "expected: %zu, got: %u\n",
+               sizeof(xMyextensionRegisterScreenLockerReq) >> 2, client->req_len);
+
+    /*
+     * HACK: i don't know exactly why this macro is called, i commented it out
+     * because it makes valid (from what i can tell) requests fail
+     * TODO: get the code to work with the macro enabled
+     */
+    /* REQUEST_SIZE_MATCH(xMyextensionRegisterScreenLockerReq); */
+
+    const char * path = (const char*)&stuff[1];
+
+    CARD8 response = 0;
+    if (exec_path && thread_id != -1) { /* already registered */
+        response = 2;
+        goto reply;
+    }
+
+    proc_id = stuff->pid;
+
+    if (!(exec_path = Xstrdup(path)))
+        return BadAlloc;
+
+    if (pthread_create(&thread_id, NULL, restart_client, NULL) != 0) {
+        free(exec_path);
+        exec_path = NULL;
+        return BadAlloc;
+    }
+
+    LogMessage(X_INFO, "registered screenlocker: '%s'\n", exec_path);
+reply:
+    xMyextensionRegisterScreenLockerReply rep = {
+        .type = X_Reply,
+        .sequenceNumber = client->sequence,
+        .response = response,
+        .exec_path_len = exec_path? strlen(exec_path) + 1: 0,
+    };
+
+    LogMessage(X_INFO, "exec len: %u\n", rep.exec_path_len);
+
+    if (client->swapped) {
+        FatalError("fixme?");
+    }
+
+    WriteToClient(client, sizeof(xMyextensionRegisterScreenLockerReply), &rep);
+    /* if (exec_path) { */
+    /*     LogMessage(X_INFO, "writing exec path\n"); */
+    /*     WriteToClient(client, rep.exec_path_len, exec_path); */
+    /* } */
+    return Success;
+}
+
+static int
+ProcMyextensionUnregisterScreenLocker(ClientPtr client) {
+    LogMessage(X_INFO, "expected: %zu, got: %d\n",
+               (sizeof(xMyextensionUnregisterScreenLockerReq) >> 2), client->req_len);
+    REQUEST_SIZE_MATCH(xMyextensionUnregisterScreenLockerReq);
+
+    if (exec_path) {
+        free(exec_path);
+        exec_path = NULL;
+    }
+
+    if (thread_id != -1) {
+        pthread_cancel(thread_id);
+        thread_id = -1;
+    }
+
+    return Success;
+}
+
 static int
 ProcScreenSaverDispatch(ClientPtr client)
 {
@@ -79,6 +195,10 @@ ProcScreenSaverDispatch(ClientPtr client)
             return lockscreen(client);
         case X_MyextensionUnlockScreen:
             return unlockscreen(client);
+        case X_MyextensionRegisterScreenLocker:
+            return ProcMyextensionRegisterScreenLocker(client);
+        case X_MyextensionUnregisterScreenLocker:
+            return ProcMyextensionUnregisterScreenLocker(client);
         default:
             return BadRequest;
     }
